00206da2624072a54e7dfe5e278a3e8955ac44e5
DEEPIKA <97511874+DeepikaClara@users.noreply.github.com>
2022-01-28 22:09:41 +0530

    Add files via upload

---
+++ do while/Dowhile.java
@@ -0,0 +1,14 @@
+package basics;
+public class Dowhile
+{
+public static void main(String args[])
+{
+	int i=7;
+	//do while loop
+	do
+		{
+		System.out.println(i);  // prints 7
+		i--;          			//decrements as 6
+		}while(i>3);			//checks condition 6>3 ->true and continues
+}
+}
